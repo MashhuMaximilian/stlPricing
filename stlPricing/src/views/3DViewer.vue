833bd510c0424a12d93817ce598faa4cf4795675
Marius Ion <ionmariusc97@gmail.com>
2023-12-12 12:37:54 +0200

    added assets

---
+++ stlPricing/src/views/3DViewer.vue
@@ -0,0 +1,24 @@
+<template>
+    <div ref="rendererContainer" style="width: 100%; height: 100%;"></div>
+  </template>
+  
+  <script>
+  import * as THREE from 'three';
+  import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+  import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+  import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js';
+  import { STLLoader } from 'three/examples/jsm/loaders/STLLoader';
+
+  export default {
+  setup() {
+    const container = ref(null);
+
+    onMounted(() => {
+      const scene = new THREE.Scene();
+    });
+
+    return {
+      container,
+    };
+  },
+};
