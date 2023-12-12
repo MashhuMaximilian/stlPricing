957a7789ab5b1a2cfb061996a995f18024e2bb5a
Marius Ion <ionmariusc97@gmail.com>
2023-12-12 12:17:51 +0200

    empty out ready to start

---
+++ stlPricing/src/views/Home.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+  HOME
+  <main>
+
+  </main>
+</template>
+<script>
+export default {
+  name: "Home",
+
+  setup(){
+    console.log("setup")
+  },
+
+}
+
+</script>
